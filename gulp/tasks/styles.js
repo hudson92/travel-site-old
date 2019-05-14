3d437d1575fbfe894a96f9342d2b5d1e936fbb20
Sam Hudson <samhudson1992@gmail.com>
2019-05-14 16:04:09 +0100

    Updates to mobile first CSS, adding in mixins postcss

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,12 +3,13 @@ postcss = require ('gulp-postcss'),
 autoprefixer = require ('autoprefixer'),
 cssvars = require ('postcss-simple-vars'),
 nested = require ('postcss-nested'),
-cssImport = require ('postcss-import');
+cssImport = require ('postcss-import'),
+mixins = require ('postcss-mixins');
     
 gulp.task('styles', function(){
     /*This is moving the code along the pipe - from source to destination - then adding in transformative programs to change the code we originally generated*/ /*This function needs return at the start so gulp is aware when it completes*/
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssImport, cssvars, nested, autoprefixer])) /*This is where the transformation of the code automates - add in any gulp plugins here to apply the postcss*/
+        .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer])) /*This is where the transformation of the code automates - add in any gulp plugins here to apply the postcss*/
         .on('error', function (errorInfo){
         console.log(errorInfo.toString());
         this.emit('end');
